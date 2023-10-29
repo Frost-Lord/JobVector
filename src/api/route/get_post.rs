2b78be1a001f3ac5171ed9bc115581c35d4ac73e
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2023-10-29 22:59:43 +1100

    Dynamic AI rendering route

--- src/api/route/get_post.rs
+++ src/api/route/get_post.rs
@@ -1,5 +1,26 @@
+use tensorflow::{Graph, SavedModelBundle, Session, SessionOptions, SessionRunArgs, Tensor, Status};
 use warp::Rejection;
 use serde::{Deserialize, Serialize};
+use std::result::Result;
+
+fn model_load() -> Result<(Graph, Session), Status> {
+    let mut graph = Graph::new();
+    let session_opts = SessionOptions::new();
+    let export_dir = "./saved_model";
+    let tags = ["serve"];
+    let bundle = SavedModelBundle::load(&session_opts, &tags, &mut graph, export_dir)?;
+    Ok((graph, bundle.session))
+}
+
+fn get_item_from_prediction(output: &[f32]) -> String {
+    let items = vec!["apple", "banana", "carrot", "donut", "egg", "fish"];
+    let max_index = output.iter()
+                          .enumerate()
+                          .max_by(|x, y| x.1.partial_cmp(y.1).unwrap())
+                          .map(|(index, _)| index)
+                          .unwrap_or(0);
+    items[max_index].to_string()
+}
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Post {
@@ -7,11 +28,30 @@ pub struct Post {
     pub body: String,
 }
 
-// A function to handle GET requests at /posts/{id}
 pub async fn get_post(id: u64) -> Result<impl warp::Reply, Rejection> {
+    let (graph, session) = model_load().unwrap();
+    
+    let data: Vec<f32> = vec![0.0, 1.0, 0.0, 0.0, 1.0, 0.0];
+    let input_tensor = Tensor::new(&[1, 6]).with_values(&data).unwrap();
+
+    let mut args = SessionRunArgs::new();
+
+    let input_op = graph.operation_by_name_required("serving_default_dense_input").unwrap();
+    args.add_feed(&input_op, 0, &input_tensor);
+
+    let output_op = graph.operation_by_name_required("StatefulPartitionedCall").unwrap();
+    let output_token = args.request_fetch(&output_op, 0);
+    
+    session.run(&mut args).unwrap();
+
+    let output_tensor: Tensor<f32> = args.fetch(output_token).unwrap();
+    let output_data: Vec<f32> = output_tensor.iter().cloned().collect();
+
+    let predicted_item = get_item_from_prediction(&output_data);
+
     let post = Post {
         id,
-        body: String::from("This is a post about Warp."),
+        body: format!("This is a post about Warp. Predicted item: {}", predicted_item),
     };
     Ok(warp::reply::json(&post))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
 use std::error::Error;
 use std::result::Result;
-use tensorflow::{Graph, SavedModelBundle, Session, SessionOptions, SessionRunArgs, Tensor, Status};
 
 mod api {
     pub mod routes;
     pub mod route {
         pub mod get_post;
     }
-}
-
-fn model_load() -> Result<(Graph, Session), Status> {
-    let mut graph = Graph::new();
-    let session_opts = SessionOptions::new();
-    let export_dir = "./saved_model";
-    let tags = ["serve"];
-    let bundle = SavedModelBundle::load(&session_opts, &tags, &mut graph, export_dir)?;
-    Ok((graph, bundle.session))
-}
-
-fn get_item_from_prediction(output: &[f32]) -> String {
-    let items = vec!["apple", "banana", "carrot", "donut", "egg", "fish"];
-    let max_index = output.iter()
-                          .enumerate()
-                          .max_by(|x, y| x.1.partial_cmp(y.1).unwrap())
-                          .map(|(index, _)| index)
-                          .unwrap_or(0);
-    items[max_index].to_string()
 }
 
 async fn run_server() -> Result<(), Box<dyn Error>> {
@@ -43,28 +23,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     .build()
     .unwrap()
     .block_on(run_server())?;
-
-    let (graph, session) = model_load()?;
-    
-    let data: Vec<f32> = vec![0.0, 1.0, 0.0, 0.0, 1.0, 0.0];
-    let input_tensor = Tensor::new(&[1, 6]).with_values(&data)?;
-
-    let mut args = SessionRunArgs::new();
-
-    let input_op = graph.operation_by_name_required("serving_default_dense_input")?;
-    args.add_feed(&input_op, 0, &input_tensor);
-
-    let output_op = graph.operation_by_name_required("StatefulPartitionedCall")?;
-    let output_token = args.request_fetch(&output_op, 0);
-    
-    session.run(&mut args)?;
-
-    let output_tensor: Tensor<f32> = args.fetch(output_token)?;
-    let output_data: Vec<f32> = output_tensor.iter().cloned().collect();
-    println!("Model output: {:?}", output_data);
-
-    let predicted_item = get_item_from_prediction(&output_data);
-    println!("Predicted item: {}", predicted_item);
 
     Ok(())
 }
