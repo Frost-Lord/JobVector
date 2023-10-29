7d085816b6e506b29bb8efd35b22a7341aee1ad5
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2023-10-30 10:51:06 +1100

    Error catching

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,11 +1,20 @@
-use warp::Filter;
+use warp::{Filter, Rejection, Reply};
+use crate::BColors;
 
-pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    get_post()
+pub async fn handle_rejection(err: Rejection) -> Result<Box<dyn Reply>, Rejection> {
+    let colors = BColors::new();
+    let message = "Internal Server Error";
+    eprintln!("{}[Rust AI]{} Unhandled Rejection:{} {:?}", colors.blue, colors.fail, colors.endc, err);
+    let json = warp::reply::json(&serde_json::json!({"error": message}));
+    Ok(Box::new(json))
 }
-
 fn get_post() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("predict" / u64)
         .and(warp::get())
         .and_then(super::route::get_post::get_post)
+}
+
+pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    get_post()
+        .recover(handle_rejection)
 }
